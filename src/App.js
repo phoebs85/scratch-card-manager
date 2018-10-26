d2c96750cde55d2bf86a4f442d15c3f7a1675695
Delson Tan <delsontan@Delsons-MacBook-Pro.local>
2018-10-26 14:01:09 -0700

    Make card grid

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
 import React, {Component} from 'react'
-import logo from './logo.svg'
 import ScratchCard from './scratch-card'
+import Flex from './flex'
+import FlexItem from './flex/flex-item'
 import './App.css'
+
+const scratchCard = (width, height, percentToFinish, subRectRatio) => (
+  <FlexItem margin="sm">
+    <ScratchCard
+      width={width}
+      height={height}
+      percentToFinish={percentToFinish}
+      subRectRatio={subRectRatio}
+    >
+      <img
+        width="150"
+        height="150"
+        src="https://jennamolby.com/wp-content/uploads/2016/08/sorry.png"
+        alt="scratch card"
+      />
+    </ScratchCard>
+  </FlexItem>
+)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <ScratchCard
-            width="150"
-            height="150"
-            percentToFinish={80}
-            subRectRatio={0.5}
-          >
-            <img
-              width="150"
-              height="150"
-              src="https://jennamolby.com/wp-content/uploads/2016/08/sorry.png"
-            />
-          </ScratchCard>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Flex>
+            {scratchCard(150, 150)}
+            {scratchCard(150, 150)}
+            {scratchCard(150, 150)}
+          </Flex>
+          <Flex>
+            {scratchCard(150, 150)}
+            {scratchCard(150, 150)}
+            {scratchCard(150, 150)}
+          </Flex>
+          <Flex>
+            {scratchCard(150, 150)}
+            {scratchCard(150, 150)}
+            {scratchCard(150, 150)}
+          </Flex>
         </header>
       </div>
     )
