3a6ec4fa67e685e2e349bfe5e1fbe8f4857bde88
Delson Tan <delsontan@Delsons-MBP.hitronhub.home>
2019-03-21 22:10:04 -0700

    Clean up server code and npm audit fix

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+
 const path = require('path')
-const app = express()
-app.use(express.static(path.join(__dirname, 'build')))
+const server = express()
+server.use(express.static(path.join(__dirname, 'build')))
 
-app.get('/ping', function(req, res) {
-  return res.send('pong')
-})
-
-app.get('/', function(req, res) {
+server.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 const port = process.env.PORT || 8080
-app.listen(port)
+server.listen(port)
 
 console.log('App is listening on port ' + port)
