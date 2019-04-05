2ade4b526f5d0064dea213758a571571d6d4913c
Delson Tan <delsontan@Delsons-MacBook-Pro.local>
2019-04-05 09:49:16 -0700

    Updates

---
+++ precache-manifest.6c02bcb7c4a251fade77bba06168be7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fedc2aae6c538562a8f9",
+    "url": "/scratch-card-manager/static/js/runtime~main.28a2f98d.js"
+  },
+  {
+    "revision": "db05a7608d4de77c31a6",
+    "url": "/scratch-card-manager/static/js/main.8bbc08c1.chunk.js"
+  },
+  {
+    "revision": "88226d297a1cae9d2478",
+    "url": "/scratch-card-manager/static/js/2.c82d4127.chunk.js"
+  },
+  {
+    "revision": "db05a7608d4de77c31a6",
+    "url": "/scratch-card-manager/static/css/main.fa01ec9b.chunk.css"
+  },
+  {
+    "revision": "88226d297a1cae9d2478",
+    "url": "/scratch-card-manager/static/css/2.b7b1ac55.chunk.css"
+  },
+  {
+    "revision": "1c88eeb7071c414735aac4100a1b1bdd",
+    "url": "/scratch-card-manager/index.html"
+  }
+];
